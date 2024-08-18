087bf79ee642b6e8604dd2ef6bc2b4bee7089563
hmwong247 <hmwong247@gmail.com>
2024-08-18 20:45:00 +0800

    amend: basic init setup

--- lua/options.lua
+++ lua/options.lua
@@ -5,7 +5,7 @@ vim.g.maplocalleader = '\\'
 vim.g.have_nerd_font = true
 
 vim.opt.number = true
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 vim.opt.mouse = 'a'
 vim.opt.showmode = false
 
