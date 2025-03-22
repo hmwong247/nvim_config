3e5c521e5cb125faa0722f043f27a66230761df0
hmwong <hmwong247@gmail.com>
2025-03-23 01:39:25 +0800

    added diffview for git review

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -3,8 +3,12 @@ vim.g.maplocalleader = "\\"
 
 vim.keymap.set("n", "<leader>tt", "<cmd>Neotree left toggle<cr>", { desc = "Toggle NeoTree" })
 vim.keymap.set("n", "<leader>buf", "<cmd>Neotree buffers<cr>", { desc = "Neotree buffer view" })
-vim.keymap.set("n", "<leader>git", "<cmd>Neotree git_status<cr>", { desc = "Neotree git view" })
+-- vim.keymap.set("n", "<leader>git", "<cmd>Neotree git_status<cr>", { desc = "Neotree git view" })
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.keymap.set("n", "<leader>dv[", "<cmd>DiffviewOpen<cr>", { desc = "DiffviewOpen" })
+vim.keymap.set("n", "<leader>dv]", "<cmd>DiffviewClose<cr>", { desc = "DiffviewClose" })
+vim.keymap.set("n", "<leader>dvh", "<cmd>DiffviewFileHistory<cr>", { desc = "DiffviewFileHistory" })
 
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
