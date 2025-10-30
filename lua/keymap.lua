b5466f0218f7fd8e0de2458b8afffc3d10ec57a8
hmwong <hmwong247@gmail.com>
2025-10-30 19:06:21 +0800

    Added autocmd to switch alternate file

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -10,7 +10,11 @@ vim.keymap.set("n", "<leader>dv[", "<cmd>DiffviewOpen<cr>", { desc = "DiffviewOp
 vim.keymap.set("n", "<leader>dv]", "<cmd>DiffviewClose<cr>", { desc = "DiffviewClose" })
 vim.keymap.set("n", "<leader>dvh", "<cmd>DiffviewFileHistory<cr>", { desc = "DiffviewFileHistory" })
 
+-- window navigation
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- switch to alternate file
+vim.keymap.set("n", "<leader>a", "<cmd>e #<CR>", { desc = "" })
